e3024f78be3db9d7324f51cbec4b33710407dc72
Andrew Curioso <andrew@ziftr.com>
2014-05-09 18:27:01 -0400

    Switch to using SASS instead of Stylus since most of the Ziftr codebase uses SASS

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var sass = require('node-sass');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -19,7 +20,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(sass.middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
